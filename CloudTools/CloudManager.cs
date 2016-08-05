705a90a262d588ef5717fef6f66213cbb24c9293
Jaime Ferrando Huertas <jferrando@ceesi.local>
2016-08-05 10:18:05 +0200

    adding files

---
+++ CloudTools/CloudManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Dropbox.Api;
+using Nito.AsyncEx;
+using System.Windows.Forms;
+
+
+
+namespace FLEXYGO
+{
+    class CloudManager
+    {
+        private DropboxClient dbx;
+        private DriveService service;
+        
+
+        public CloudManager(string user,string jsonpath)//Drive
+        {
+            service = Drive.GetGoogleOAuthCredential(user,jsonpath);
+        }
+        public CloudManager(string firstHalfToken)//Dropbox auth builder, need the uri from getFirstHalfToken
+        {
+            dbx = dropbox.secondAuthUser(firstHalfToken);
+        }
+        public static Uri getFirstHalfToken()
+        {
+            return dropbox.firstAuthUser();
+        }
+        public Boolean downloadFile(Object autenti,string fileNameOrID,string pathTosave, string folderDropbox = "")
+        {
+            try
+            {
+                if (autenti == dbx) { dropbox.downloadFile(dbx, fileNameOrID, folderDropbox, pathTosave); }
+                else if(autenti == service) { Drive.downloadBinaryFile(service, fileNameOrID, pathTosave); }
+                return true;
+            }
+            catch(Exception e) { return false; }
+        }
+        public Boolean updateFile(Object autenti, string filePath, string nameFinal, string IDParentorFolderDropbox = "")
+        {
+            try
+            {
+                if (autenti == dbx) { dropbox.uploadFile(dbx,filePath,IDParentorFolderDropbox,nameFinal); }
+                else if (autenti == service) { Drive.uploadFile(service, filePath, IDParentorFolderDropbox, nameFinal); }
+                return true;
+            }
+            catch (Exception e) { return false; }
+        }
+        public Boolean createFolder(Object autenti,string name, string parentIDORdropboxPath)
+        {
+            try
+            {
+                if (autenti == dbx) { dropbox.createFolder(dbx, name, parentIDORdropboxPath); }
+                else if (autenti == service) { Drive.createDirectory(service, name, parentIDORdropboxPath); }
+                return true;
+            }
+            catch(Exception e) { return false; }
+        }
+        public string getMimeType(string filename)
+        {
+           return  Drive.GetMimeType(filename);
+        }
+        public List<CloudObject> getFiles(Object autenti, string folderDB)
+        {
+            try
+            {
+                if (autenti == dbx) { return dropbox.retrieveFolder(dbx, folderDB); }
+                else if (autenti == service) { return Drive.getAllFiles(service); }
+                return null;
+            }
+            catch(Exception e) { return null; }
+        }
+
+    }
+}
---
+++ CloudTools/CloudObject.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Dropbox.Api;
+using Nito.AsyncEx;
+using System.Windows.Forms;
+namespace FLEXYGO
+{
+    class CloudObject
+    {
+        public string Name;
+        public Boolean isDropbox;
+        public Boolean isDrive;
+        //Aboce this is dropbox attributes
+        public Dropbox.Api.Files.DeletedMetadata AsDeleted;
+        public Dropbox.Api.Files.FileMetadata AsFile;
+        public Dropbox.Api.Files.FolderMetadata AsFolder;
+        public bool IsDeleted;
+        public bool IsFile;
+        public bool IsFolder;
+        public string ParentSharedFolderId;
+        public string PathDisplay;
+        public string PathLower;
+
+        //Above this is drive attributes
+        public virtual IDictionary<string, string> AppProperties { get; set; }
+        public File.CapabilitiesData Capabilities;
+        public string originalFileName;
+        public File.ContentHintsData ContentHints;
+        public DateTime? CreatedTime;
+        public string CreatedTimeRaw;
+        public string Description;
+        public string ETag;
+        public bool? ExplicitlyTrashed;
+        public string FileExtension;
+        public string FolderColorRgb;
+        public string FullFileExtension;
+        public string HeadRevisionId;
+        public string IconLink;
+        public string Id;
+        public File.ImageMediaMetadataData ImageMediaMetadata;
+        public Boolean? IsAppAuthorized;
+        public string Kind;
+        public User LastModifyingUser;
+        public string Md5Checksum;
+        public string MimeType;
+        public DateTime? ModifiedByMeTime;
+        public string ModifiedByMeTimeRaw;
+        public DateTime? ModifiedTime;
+        public string ModifiedTimeRaw;
+        public string OriginalFilename;
+        public Boolean? OwnedByMe;
+        public IList<User> Owners;
+        public IList<string> Parents;
+        public IList<Permission> Permissions;
+        public IDictionary<string, string> Properties;
+        public long? QuotaBytesUsed;
+        public Boolean? Shared;
+        public DateTime? SharedWithMeTime;
+        public string SharedWithMeTimeRaw;
+        public User SharingUser;
+        public long? Size;
+        public IList<string> Spaces;
+        public Boolean? Starred;
+        public string ThumbnailLink;
+        public Boolean? Trashed;
+        public long? Version;
+        public File.VideoMediaMetadataData VideoMediaMetadata;
+        public Boolean? ViewedByMe;
+        public DateTime? ViewedByMeTime;
+        public string ViewedByMeTimeRaw;
+        public Boolean? ViewersCanCopyContent;
+        public string WebContentLink;
+        public string WebViewLink;
+        public Boolean? WritersCanShare;
+        //
+
+        public CloudObject(Dropbox.Api.Files.Metadata db)
+        {
+            Name = db.Name;
+            isDrive = false;
+            isDropbox = true;
+            AsDeleted = db.AsDeleted;
+            AsFile = db.AsFile;
+            AsFolder = db.AsFolder;
+            IsDeleted = db.IsDeleted;
+            IsFile = db.IsFile;
+            IsFolder = db.IsFolder;
+            ParentSharedFolderId = db.ParentSharedFolderId;
+            PathDisplay = db.PathDisplay;
+            PathLower = db.PathLower;
+        }
+        public CloudObject(Google.Apis.Drive.v3.Data.File drive)
+        {
+            Name = drive.Name;
+            originalFileName = drive.OriginalFilename;
+            Capabilities = drive.Capabilities;
+            ContentHints = drive.ContentHints;
+            CreatedTime = drive.CreatedTime;
+            CreatedTimeRaw = drive.CreatedTimeRaw;
+            Description = drive.Description;
+            ETag = drive.ETag;
+            ExplicitlyTrashed = drive.ExplicitlyTrashed;
+            FileExtension = drive.FileExtension;
+            FolderColorRgb = drive.FolderColorRgb;
+            FullFileExtension = drive.FullFileExtension;
+            HeadRevisionId = drive.HeadRevisionId;
+            IconLink = drive.IconLink;
+            Id = drive.Id;
+            ImageMediaMetadata = drive.ImageMediaMetadata;
+            IsAppAuthorized = drive.IsAppAuthorized;
+            Kind = drive.Kind;
+            LastModifyingUser = drive.LastModifyingUser;
+            Md5Checksum = drive.Md5Checksum;
+            MimeType = drive.MimeType;
+            ModifiedByMeTime = drive.ModifiedByMeTime;
+            ModifiedByMeTimeRaw=drive.ModifiedByMeTimeRaw;
+            ModifiedTime = drive.ModifiedTime;
+            ModifiedTimeRaw = drive.ModifiedTimeRaw;
+            originalFileName = drive.OriginalFilename;
+            OwnedByMe = drive.OwnedByMe;
+            Owners = drive.Owners;
+            Parents = drive.Parents;
+            Permissions = drive.Permissions;
+            Properties = drive.Properties;
+            QuotaBytesUsed = drive.QuotaBytesUsed;
+            Shared = drive.Shared;
+            SharedWithMeTime = drive.SharedWithMeTime;
+            SharedWithMeTimeRaw = drive.SharedWithMeTimeRaw;
+            SharingUser = drive.SharingUser;
+            Size = drive.Size;
+            Spaces = drive.Spaces;
+            Starred = drive.Starred;
+            ThumbnailLink = drive.ThumbnailLink;
+            Trashed = drive.Trashed;
+            Version = drive.Version;
+            VideoMediaMetadata = drive.VideoMediaMetadata;
+            ViewedByMe = drive.ViewedByMe;
+            ViewedByMeTime = drive.ViewedByMeTime;
+            ViewedByMeTimeRaw = ViewedByMeTimeRaw;
+            ViewersCanCopyContent = drive.ViewersCanCopyContent;
+            WebContentLink = drive.WebContentLink;
+            WebViewLink = drive.WebViewLink;
+            WritersCanShare = drive.WritersCanShare;
+
+        }
+    }
+}
