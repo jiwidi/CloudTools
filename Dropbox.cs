b26210ea919745889d2ae1a0e047d5e81dd73780
Jaime Ferrando Huertas <fhjaime96@gmail.com>
2017-05-08 17:46:22 +0200

    Update Dropbox.cs

--- Dropbox.cs
+++ Dropbox.cs
@@ -13,8 +13,8 @@ namespace CloudTools
      public class dropbox //: CloudDocInterface
     {
         //public DropboxClient dbx; //DropboxClient needed to work in almost every method, instanciated by secondAuthUser(token got from the url of firstAuthUser)
-        static string ClientId = "3dj3sbjdiyentjq"; //App key
-        static string ClientSecret = "94siutu71pyz2ad"; //App secret key
+        static string ClientId = ""; //App key
+        static string ClientSecret = ""; //App secret key
 
 
 
